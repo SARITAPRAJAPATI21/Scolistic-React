7c1c9da73a177f911c078f76493810d44d40fecb
unknown <saritapra931@gmail.com>
2024-01-01 12:09:59 +0530

    gh-pages setup

---
+++ client/build/precache-manifest.a899336211ab4627eb268fcfa08fd9f8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c2ac108efafa534d960",
+    "url": "/static/css/main.a21ca358.chunk.css"
+  },
+  {
+    "revision": "4c2ac108efafa534d960",
+    "url": "/static/js/main.4c2ac108.chunk.js"
+  },
+  {
+    "revision": "8a5b0b70b12187e5da28",
+    "url": "/static/js/1.8a5b0b70.chunk.js"
+  },
+  {
+    "revision": "6d0168dd101359e66ebb",
+    "url": "/static/js/2.6d0168dd.chunk.js"
+  },
+  {
+    "revision": "a46ef3f558368343807d",
+    "url": "/static/js/runtime~main.a46ef3f5.js"
+  },
+  {
+    "revision": "864732a40cf60ddbf25d82359e216724",
+    "url": "/static/media/loginpic.864732a4.png"
+  },
+  {
+    "revision": "12e12bea6d825b4eaba148dba171326e",
+    "url": "/static/media/signup.12e12bea.png"
+  },
+  {
+    "revision": "ee0bcbb084236c4ce5b3222683e41456",
+    "url": "/index.html"
+  }
+];
